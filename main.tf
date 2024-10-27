fd886ea0c16bd9f179daa10a84e2d7f5365b8660
Ayolowo <58194998+Ayolowo@users.noreply.github.com>
2024-10-28 00:54:18 +0100

    Python practice

---
+++ main.tf
@@ -0,0 +1,48 @@
+# This create an S3 bucket to store movie images
+resource "aws_s3_bucket" "bucket_movies" {
+  bucket = "my-movie-images-bucket"
+
+  tags = {
+    Name = "Movie images"
+  }
+}
+
+# A DynamoDB table to store movie data
+resource "aws_dynamodb_table" "movies-table" {
+  name           = "Movies"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+  attribute {
+    name = "TopScore"
+    type = "N"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = true
+  }
+
+  global_secondary_index {
+    name               = "GameTitleIndex"
+    hash_key           = "GameTitle"
+    range_key          = "TopScore"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["UserId"]
+  }
+}
