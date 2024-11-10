2c8573762833ad2617539e367b66ccd16b20c1d5
Ayolowo <58194998+Ayolowo@users.noreply.github.com>
2024-11-10 20:40:10 +0100

    import lambda role and updated code explanations

--- main.tf
+++ main.tf
@@ -12,16 +12,11 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
+# IAM role for aws Lambda to assume
 resource "aws_iam_role" "iam_role_lambda" {
   name               = "iam_role_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
-
-# data "archive_file" "lambda" {
-#   type        = "zip"
-#   source_file = "${path.module}/app/app.py"
-#   output_path = "${path.module}/app/app.zip"
-# }
 
 resource "aws_lambda_function" "terraform_function" {
   # If the file is not in the current working directory you will need to include a
@@ -37,8 +32,3 @@ resource "aws_lambda_function" "terraform_function" {
   depends_on = [aws_iam_role.iam_role_lambda]
 }
 
-# To get logs of our lambda functions | retention days for how long to keep logs
-# resource "aws_cloudwatch_log_group" "lambda_log_group" {
-#   name              = "/aws/lambda/hello_world_lambda"
-#   retention_in_days = 14
-# }
